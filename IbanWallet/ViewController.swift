f4abb8a9d8933a0bd2df58d4b969b8f071becaff
joaopdcgarcia <joaopdcgarcia@gmail.com>
2018-11-08 09:40:24 +0000

    Initial Commit

---
+++ IbanWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IbanWallet
+//
+//  Created by Joao Garcia on 11/8/18.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
